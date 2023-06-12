72452375644d382d319f0873b8b014f68757f0ae
Victoria M <vmorris517@gmail.com>
2023-06-11 23:07:22 -0500

    all tests are passing but except the ability to view jobs from the index page

--- queries.sql
+++ queries.sql
@@ -6,14 +6,21 @@ WHERE table_name = 'job';
 -- Part 2: Test it with SQL
 SELECT name
 FROM employer
-WHERE location = 'St. Louis City';
+WHERE location = "St. Louis City";
 
 -- Part 3: Test it with SQL
 DROP TABLE job;
 -- Part 4: Test it with SQL
-SELECT name
-FROM skill s
-INNER JOIN job_skills js ON s.id = js.skills_id
-WHERE js.skills_id IS NOT NULL
-ORDER BY s.name ASC;
+
+SELECT *
+FROM skill
+INNER JOIN job_skills ON skill.id = job_skills.skills_id
+WHERE job_skills.jobs_id IS NOT NULL
+ORDER BY name ASC;
+
+--SELECT name
+--FROM skill s
+--INNER JOIN job_skills js ON s.id = js.skills_id
+--WHERE js.skills_id IS NOT NULL
+--ORDER BY s.name ASC;
 
